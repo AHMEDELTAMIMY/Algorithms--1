b9aa1482a25d5b95a35feafd8aa2001cdb3955f5
AHMED ELTAMIMY <79113610+AHMEDELTAMIMY@users.noreply.github.com>
2021-03-17 20:45:33 +0100

    First comment

---
+++ js.js
@@ -0,0 +1,16 @@
+//1
+
+// 1.write numbers from 1 to 135
+// 2.print this numbers
+
+for (var i=1; i < 136; i++){
+    console.log(i)
+}
+
+
+// i=1 =>1
+// i=2 =>1,2
+// i=3 =>1,2,3
+// i=4 =>1,2,3,4
+// ..........
+// i=135 =>1,2,3,4,....135
